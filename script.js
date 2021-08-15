ffffdbcfc3e9d5168eef05322da6582581bed575
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-08-15 03:52:28 -0300

    update welcome

--- script.js
+++ script.js
@@ -1,55 +1,86 @@
+/*-----
+Spanizer
+- Wraps letters with spans, for css animations
+-----*/
+/* ********** Welcome ********** */
+((d) => {
+  var s = {};
+  const spanizeLetters = {
+    settings: {
+      letters: document.querySelectorAll('.js-spanize'),
+    },
+    init: function () {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function () {
+      s.letters[0].innerHTML = ((el) => {
+        //spanizeLetters.joinChars();
+        var spanizer = el.trim().split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      })(s.letters[0].innerHTML);
+      s.letters[1].innerHTML = ((el) => {
+        //spanizeLetters.joinChars();
+        var spanizer = el.trim().split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      })(s.letters[1].innerHTML);
+    },
+  };
+  spanizeLetters.init();
+})(document);
+
 /* ********** Menu ********** */
 ((d) => {
-    const $btnMenu = d.querySelector(".menu-btn"),
-      $menu = d.querySelector(".menu");
-  
-    $btnMenu.addEventListener("click", (e) => {
-      $btnMenu.firstElementChild.classList.toggle("none");
-      $btnMenu.lastElementChild.classList.toggle("none");
-      $menu.classList.toggle("is-active");
-    });
-  
-    d.addEventListener("click", (e) => {
-      if (!e.target.matches(".menu a")) return false;
-  
-      $btnMenu.firstElementChild.classList.remove("none");
-      $btnMenu.lastElementChild.classList.add("none");
-      $menu.classList.remove("is-active");
-    });
-  })(document);
-  
-  /* ********** ContactForm ********** */
-  ((d) => {
-    const $form = d.querySelector(".contact-form"),
-      $loader = d.querySelector(".contact-form-loader"),
-      $response = d.querySelector(".contact-form-response");
-  
-    $form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      $loader.classList.remove("none");
-      fetch("https://formsubmit.co/ajax/eduardo.salinas1@outlook.com", {
-        method: "POST",
-        body: new FormData(e.target),
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* ********** ContactForm ********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/eduardo.salinas1@outlook.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
       })
-        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-        .then((json) => {
-          console.log(json);
-          location.hash = "#gracias";
-          $form.reset();
-        })
-        .catch((err) => {
-          console.log(err);
-          let message =
-            err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
-          $response.querySelector(
-            "h3"
-          ).innerHTML = `Error ${err.status}: ${message}`;
-        })
-        .finally(() => {
-          $loader.classList.add("none");
-          setTimeout(() => {
-            location.hash = "#close";
-          }, 3000);
-        });
-    });
-  })(document);
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
